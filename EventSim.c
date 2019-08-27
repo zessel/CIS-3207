72337e59730366d7f9411b3f65c71fe3ed0fb2e5
zessel <zachary.essel@gmail.com>
2019-08-27 11:44:40 -0400

    Start work on initializers

---
+++ EventSim.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+void main ()
+{
+    char inputfilename[] = "initializers.dat";
+    char outputfilename[] = "eventoutput.dat";
+    FILE *input;
+    FILE *output;
+    input = fopen(inputfilename, "r");
+    if (input == NULL)
+    {
+        printf("The file %s was not found", inputfilename);
+    }
+    output = fopen(outputfilename, "w");
+
+    int SEED,
+        INIT_TIME,
+        FIN_TIME,
+        ARRIVE_MIN,
+        ARRIVE_MAX,
+        QUIT_PROB,
+        CPU_MIN,
+        CPU_MAX,
+        DISK1_MIN,
+        DISK1_MAX,
+        DISK2_MIN,
+        DISK2_MAX;
+
+    char emptystring[20];
+    fscanf(input, "%*s %d", &SEED);
+    fscanf(input, "%*s %d", &INIT_TIME);
+    fscanf(input, "%*s %d", &FIN_TIME);
+    fscanf(input, "%*s %d", &ARRIVE_MIN);
+    fscanf(input, "%*s %d", &ARRIVE_MAX);
+    fscanf(input, "%*s %d", &QUIT_PROB);
+    fscanf(input, "%*s %d", &CPU_MIN);
+    fscanf(input, "%*s %d", &CPU_MAX);
+    fscanf(input, "%*s %d", &DISK1_MIN);
+    fscanf(input, "%*s %d", &DISK1_MAX);
+    fscanf(input, "%*s %d", &DISK2_MIN);
+    fscanf(input, "%*s %d", &DISK2_MAX);
+
+    printf("\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n", SEED,INIT_TIME,FIN_TIME,ARRIVE_MIN,
+        ARRIVE_MAX,
+        QUIT_PROB,
+        CPU_MIN,
+        CPU_MAX,
+        DISK1_MIN,
+        DISK1_MAX,
+        DISK2_MIN,
+        DISK2_MAX);
+
+}
+
