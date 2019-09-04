ed78af3edc40e475c7cbdc21d6eb540e31dd54eb
zessel <zachary.essel@gmail.com>
2019-09-04 11:40:50 -0400

    start the case statement in main body

--- EventSim.c
+++ EventSim.c
@@ -10,13 +10,14 @@ struct process
 };
 
 /*  Event types:
-    0: Start and end simulation events
+    0: Start simulation events
     1: Arrive at CPU queue
     2: Leave CPU
     3: Arrive at Disk 1
     4: Leave Disk 1
     5: Arrive at Disk 2
     6: Leave Disk 2
+    9: End simulation events
 */
 struct event
 {
@@ -27,53 +28,40 @@ struct event
     struct event *next;
 };
 
+void initialize_from_file();
 int ranged_rand(int max, int min);
 struct event* popevent(struct event **event_queue_root);
 void print_event(struct event *current_event);
 struct event* create_event(char* id, int time, int type);
 void sorted_event_enqueue(struct event **event_queue_root, struct event *new_event);
+void enqueue(struct process **queue_tail, struct process *new_process);
+struct process* dequeue(struct process **queue_head);
+void process_arrival(struct process **cpu_queue_tail, char* id);
+void cpu_finish(struct process **cpu_queue_head, struct process **disk_tail);
 
+
+
+int SEED,
+    INIT_TIME,
+    FIN_TIME,
+    ARRIVE_MIN,
+    ARRIVE_MAX,
+    QUIT_PROB,
+    CPU_MIN,
+    CPU_MAX,
+    DISK1_MIN,
+    DISK1_MAX,
+    DISK2_MIN,
+    DISK2_MAX;
 
 
 void main ()
 {
-    char inputfilename[] = "initializers.dat";
+    initialize_from_file();
+
     char outputfilename[] = "eventoutput.dat";
-    FILE *input;
     FILE *output;
-    input = fopen(inputfilename, "r");
-    if (input == NULL)
-    {
-        printf("The file %s was not found", inputfilename);
-    }
     output = fopen(outputfilename, "w");
-
-    int SEED,
-        INIT_TIME,
-        FIN_TIME,
-        ARRIVE_MIN,
-        ARRIVE_MAX,
-        QUIT_PROB,
-        CPU_MIN,
-        CPU_MAX,
-        DISK1_MIN,
-        DISK1_MAX,
-        DISK2_MIN,
-        DISK2_MAX;
-
-    char emptystring[20];
-    fscanf(input, "%*s %d", &SEED);
-    fscanf(input, "%*s %d", &INIT_TIME);
-    fscanf(input, "%*s %d", &FIN_TIME);
-    fscanf(input, "%*s %d", &ARRIVE_MIN);
-    fscanf(input, "%*s %d", &ARRIVE_MAX);
-    fscanf(input, "%*s %d", &QUIT_PROB);
-    fscanf(input, "%*s %d", &CPU_MIN);
-    fscanf(input, "%*s %d", &CPU_MAX);
-    fscanf(input, "%*s %d", &DISK1_MIN);
-    fscanf(input, "%*s %d", &DISK1_MAX);
-    fscanf(input, "%*s %d", &DISK2_MIN);
-    fscanf(input, "%*s %d", &DISK2_MAX);
 
     printf("\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n", SEED,INIT_TIME,FIN_TIME,ARRIVE_MIN,
         ARRIVE_MAX,
@@ -127,7 +115,7 @@ void main ()
 
         switch(current_event->eventtype)
         {
-        case 0:
+        case 0: create_event(eventcount, globaltime + ranged_rand(ARRIVE_MAX,ARRIVE_MIN), 1);
             break;
         case 1: process_arrival(&cpu_queue_tail, current_event->eventid);
             break;
@@ -145,6 +133,31 @@ void main ()
 
     }
     
+}
+
+void initialize_from_file()
+{
+        char inputfilename[] = "initializers.dat";
+    FILE *input;
+
+    input = fopen(inputfilename, "r");
+    if (input == NULL)
+    {
+        printf("The file %s was not found", inputfilename);
+    }
+
+        fscanf(input, "%*s %d", &SEED);
+    fscanf(input, "%*s %d", &INIT_TIME);
+    fscanf(input, "%*s %d", &FIN_TIME);
+    fscanf(input, "%*s %d", &ARRIVE_MIN);
+    fscanf(input, "%*s %d", &ARRIVE_MAX);
+    fscanf(input, "%*s %d", &QUIT_PROB);
+    fscanf(input, "%*s %d", &CPU_MIN);
+    fscanf(input, "%*s %d", &CPU_MAX);
+    fscanf(input, "%*s %d", &DISK1_MIN);
+    fscanf(input, "%*s %d", &DISK1_MAX);
+    fscanf(input, "%*s %d", &DISK2_MIN);
+    fscanf(input, "%*s %d", &DISK2_MAX);
 }
 
 /*  Exists to slightly clean up the code when a random is called  */
@@ -240,4 +253,15 @@ void process_arrival(struct process **cpu_queue_tail, char* id)
 {
     struct process *new_process = create_process(id);
     enqueue(cpu_queue_tail, new_process);
+}
+
+void cpu_finish(struct process **cpu_queue_head, struct process **disk_tail)
+{
+    struct process *finished_process = dequeue(cpu_queue_head);
+    if (ranged_rand(0,100) < (QUIT_PROB))
+        free(finished_process);
+    else
+    {
+        enqueue(disk_tail, finished_process);
+    }
 }
