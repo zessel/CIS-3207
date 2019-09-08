10c1bd3224e99267e8140cb8d34cbde77c684637
zessel <zachary.essel@gmail.com>
2019-09-08 16:04:38 -0400

    Fix MANY segfaults... finally

--- EventSim.c
+++ EventSim.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 struct process
 {
@@ -10,9 +11,8 @@ struct process
 };
 
 /*  Event types:
-    0: Start simulation events
-    1: New process in simulation
-    2: Arrive at CPU queue
+    1: Start simulation events
+    2: New process in simulation
     3: Leave CPU
     4: Arrive at Disk 1
     5: Leave Disk 1
@@ -33,11 +33,14 @@ void initialize_from_file();
 int ranged_rand(int max, int min);
 struct event* popevent(struct event **event_queue_root);
 void print_event(struct event *current_event);
+void print_created(struct event *current_event);
 struct event* create_event(char* id, int time, int type);
 void sorted_event_enqueue(struct event **event_queue_root, struct event *new_event);
+struct process* create_process(char* id);
 void enqueue(struct process **queue_tail, struct process *new_process);
 struct process* dequeue(struct process **queue_head);
 void process_arrival(struct process **cpu_queue_tail, char* id);
+void cpu_start(struct process **cpu_queue_head, struct process **disk_tail);
 void cpu_finish(struct process **cpu_queue_head, struct process **disk_tail);
 
 
@@ -89,11 +92,12 @@ void main ()
 
     struct event *event_queue_root = NULL;
     struct event *current_event = NULL;
+    struct event *new_event = NULL;
 
     struct event *start_event = (struct event*) malloc(sizeof(struct event));
     strcpy(start_event->eventid, "START");
     start_event->poptime = INIT_TIME;
-    start_event->eventtype = 0;
+    start_event->eventtype = 1;
     start_event->prev = NULL;
 
     struct event *end_event = (struct event*) malloc(sizeof(struct event));
@@ -111,29 +115,64 @@ void main ()
     while (endhit != 1)
     {
         current_event = popevent(&event_queue_root);
+        fprintf(output, "PROCESS %s is %d\n\n", current_event->eventid, current_event->eventtype);
+     //   fclose(output);
+        print_event(current_event);
+        printf("Print successful\n");
         globaltime = current_event->poptime;
         if (strcmp(current_event->eventid, "END") == 0)
             endhit = 1;
         printf("Pop successful");
-
+        sprintf(id_as_str, "%d", processcount);
+        sleep(1);
+        
         switch(current_event->eventtype)
         {
-        case 0: create_event(processcount, globaltime + ranged_rand(ARRIVE_MAX,ARRIVE_MIN), 1);
+        case 1: 
+        printf("\nCASE 1\n");
+        sleep(1);
+        new_event = create_event(id_as_str, globaltime + ranged_rand(ARRIVE_MAX,ARRIVE_MIN), 2);
+
+                print_created(new_event);
+
+            sorted_event_enqueue(&event_queue_root, new_event);
             processcount++;
             break;
-        case 1: process_arrival(&cpu_queue_tail, current_event->eventid);
+        case 2: 
+                printf("\nCASE 2\n");
+        sleep(1);process_arrival(&cpu_queue_tail, current_event->eventid);
+            current_process = create_process(current_event->eventid);
+            enqueue(&cpu_queue_tail, current_process);
             if (cpu_queue_head == NULL)
             {
                 cpu_queue_head = cpu_queue_tail;
-                create_event(processcount, globaltime, 2);
+                new_event = create_event(current_event->eventid, globaltime + ranged_rand(CPU_MAX, CPU_MIN), 3);
+                
+
+                print_created(new_event);
+
+
+                sorted_event_enqueue(&event_queue_root, new_event);
             }
+            new_event = create_event(id_as_str, globaltime + ranged_rand(ARRIVE_MAX,ARRIVE_MIN), 2);
+            
+
+                print_created(new_event);
+
+
+            sorted_event_enqueue(&event_queue_root, new_event);
+            processcount++;
             break;
-        case 2: create_event(cpu_queue_head->processid, globaltime + ranged_rand(CPU_MAX,CPU_MIN), 3);
-            break;
-        case 3: current_process = dequeue(&cpu_queue_head);
-            create_event(cpu_queue_head->processid, globaltime, 2);
+        case 3: 
+        
+                printf("\nCASE 3\n");
+        sleep(1);current_process = dequeue(&cpu_queue_head);
+//            new_event = create_event(cpu_queue_head->processid, globaltime, 2);
+ //           sorted_event_enqueue(&event_queue_root, new_event);
             if (ranged_rand(0,100) < (QUIT_PROB))
+            {
                 free(current_process);
+            }
             else
             {
                 if (disk1_queue_count >= disk2_queue_count)
@@ -141,8 +180,14 @@ void main ()
                     enqueue(&disk1_queue_tail, current_process);
                     if (disk1_queue_head == NULL)
                     {
-                        disk1_queue_head = current_process;
-                        create_event(current_process->processid, globaltime, 4);
+                        disk1_queue_head = current_process;                      
+                        new_event = create_event(current_process->processid, globaltime, 4);
+                        
+
+                print_created(new_event);
+
+
+                        sorted_event_enqueue(&event_queue_root, new_event);
                     }
                     disk1_queue_count++;
                 }
@@ -152,44 +197,91 @@ void main ()
                     if (disk2_queue_head == NULL)
                     {
                         disk2_queue_head = current_process;
-                        create_event(current_process->processid, globaltime, 6);
+                        new_event = create_event(current_process->processid, globaltime, 6);
+                        
+
+                print_created(new_event);
+
+
+                        sorted_event_enqueue(&event_queue_root, new_event);
                     }
                     disk2_queue_count++;
                 }
             }
             break;
-        case 4: create_event(disk1_queue_head->processid, globaltime + ranged_rand(DISK1_MAX,DISK1_MIN), 5);
+        case 4: 
+        
+                printf("\nCASE 4\n");
+        sleep(1);new_event = create_event(disk1_queue_head->processid, globaltime + ranged_rand(DISK1_MAX,DISK1_MIN), 5);
+        
+
+                print_created(new_event);
+
+
+            sorted_event_enqueue(&event_queue_root, new_event);
             break;
-        case 5: current_process = dequeue(&disk1_queue_tail);
-            create_event(disk1_queue_head->processid, globaltime, 4);
-            enqueue(&cpu_queue_tail, current_process);
-            if (cpu_queue_head == NULL)
+        case 5: 
+        
+                printf("\nCASE 5\n");
+        sleep(1);current_process = dequeue(&disk1_queue_head);
+            if (disk1_queue_head != NULL)
             {
-                cpu_queue_head = cpu_queue_tail;
-                create_event(current_process->processid, globaltime, 2);
+                new_event = create_event(disk1_queue_head->processid, globaltime, 4);
+            
+
+                print_created(new_event);
+
+            
+                sorted_event_enqueue(&event_queue_root, new_event);
             }
-        case 6: create_event(disk2_queue_head->processid, globaltime + ranged_rand(DISK2_MAX,DISK2_MIN), 7);
-            break;
-        case 7: current_process = dequeue(&disk2_queue_tail);
-            create_event(disk2_queue_head->processid, globaltime, 6);
             enqueue(&cpu_queue_tail, current_process);
             if (cpu_queue_head == NULL)
             {
                 cpu_queue_head = cpu_queue_tail;
-                create_event(current_process->processid, globaltime, 2);
+            }
+            break;
+        case 6: 
+        
+                printf("\nCASE 6\n");
+        sleep(1);new_event = create_event(disk2_queue_head->processid, globaltime + ranged_rand(DISK2_MAX,DISK2_MIN), 7);
+        
+
+                print_created(new_event);
+
+
+            sorted_event_enqueue(&event_queue_root, new_event);
+            break;
+        case 7: 
+        
+                printf("\nCASE 7\n");
+        sleep(1);current_process = dequeue(&disk2_queue_head);
+        if (disk2_queue_head != NULL)
+        {
+            new_event = create_event(disk2_queue_head->processid, globaltime, 6);
+            
+
+                print_created(new_event);
+
+
+            sorted_event_enqueue(&event_queue_root, new_event);
+        }
+            enqueue(&cpu_queue_tail, current_process);
+            if (cpu_queue_head == NULL)
+            {
+                cpu_queue_head = cpu_queue_tail;
             }        
-        default:
+        default:        printf("\nCASE DEFAULT\n");
+        sleep(1);
             break;
         }
-        print_event(current_event);
-        printf("Print successful\n");
- /*       sprintf(str, "%d", processcount);
-        sorted_event_enqueue(&event_queue_root, create_event(str, 
-            globaltime + ranged_rand(ARRIVE_MAX,ARRIVE_MIN), 1)); */
+ //       sprintf(str, "%d", processcount);
+ //       sorted_event_enqueue(&event_queue_root, create_event(str, 
+ //           globaltime + ranged_rand(ARRIVE_MAX,ARRIVE_MIN), 1)); 
         free(current_event);
-        printf("Free successful\n");
+        printf("Free successful\n  END OF LOOP \n\n********\n\n");
 
     }
+    fclose(output);
     
 }
 
@@ -240,7 +332,19 @@ struct event* popevent(struct event **event_queue_root)
 void print_event(struct event *current_event)
 {
     printf("\nThe event id is %s", current_event->eventid);
-    printf("\nThe Event time is %d\n", current_event->poptime);
+    printf("\nThe event time is %d", current_event->poptime);
+    printf("\nThe event type is %d", current_event->eventtype);
+    printf("\nDONE PRINTING\n");
+    sleep(1);
+}
+
+void print_created(struct event *current_event)
+{
+    printf("\n*****The event id is %s", current_event->eventid);
+    printf("\n*****The event time is %d", current_event->poptime);
+    printf("\n*****The event type is %d", current_event->eventtype);
+    printf("\n*****DONE PRINTING\n");
+    sleep(1);
 }
 
 struct event* create_event(char* id, int time, int type)
@@ -291,12 +395,19 @@ void sorted_event_enqueue(struct event **event_queue_root, struct event *new_eve
 struct process* create_process(char* id)
 {
     struct process *new_process = (struct process*) malloc(sizeof(struct process));
+    strcpy(new_process->processid, id); 
     return new_process;
 }
 
 void enqueue(struct process **queue_tail, struct process *new_process)
 {
-    (*queue_tail)->next = new_process;
+    if ((*queue_tail) != NULL)
+        (*queue_tail)->next = new_process;
+    else
+    {
+        (*queue_tail = new_process);
+    }
+    
 }
 
 struct process* dequeue(struct process **queue_head)
@@ -328,5 +439,3 @@ void cpu_finish(struct process **cpu_queue_head, struct process **disk_tail)
         enqueue(disk_tail, finished_process);
     }
 }
-
- 
