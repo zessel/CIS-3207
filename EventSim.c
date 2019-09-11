289197ae29bf487b24e64d08e00475b04d6ecb0a
zessel <zachary.essel@gmail.com>
2019-09-11 15:15:33 -0400

    Add comments and TODOs

--- EventSim.c
+++ EventSim.c
@@ -1,8 +1,32 @@
+/*  Zach Essel
+    09/11/2019
+    CIS 3207 001
+    Project 1: Giorgio's Discrete Event Simulator
+
+    The goal of this project is to simulate virtualization of the cpu.  This is accomplished by having processes
+    gain the cpu for some random amount of time before either exiting or moving on to perform i/o where they then
+    reenter the cpu queue.  The simulator is not quite time driven, but instead driven by a queue of events which occur
+    chronologically.  Events are created according to a random time interval and also throughout the running of the program.
+    After creation events are inserted into the event queue sorted.  Events themselves create, move, and free process nodes.
+
+    The main program is a loop that pops the top event, outputs and performs its associated actions, frees the event, then
+    restarts the loop.
+
+    The parameters the program runs on are imported from a file "initializers.dat" and the logs are output to a file "eventoutput.dat"
+    eventoutput.dat will be created if it does not exist. 
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+
+/*  This is the structure for the processes that go between cpu and i/o
+    the contain an processid stored as a string, because no mathematical
+    operations are done using it.  They also contain a process pointer
+    so that they can be queued up
+*/
 
 struct process
 {
@@ -10,7 +34,14 @@ struct process
     struct process *next;     
 };
 
-/*  Event types:
+/*  This is the structure for events which make up the priority queue.
+    They have previous and next event pointers, so they are actually more of a linked list.
+    eventid is a string that associates the event with a process        TODO: rename this
+    poptime is the global time at which the event occurs
+    eventtype signifies the action that the process action the event is associated with
+
+    Event types:
+    0: End simulation events
     1: Start simulation events
     2: New process in simulation
     3: Enter CPU
@@ -19,8 +50,7 @@ struct process
     6: Leave Disk 1
     7: Arrive at Disk 2
     8: Leave Disk 2
-    Y: Exit System
-    0: End simulation events
+    9: Exit System
 */
 struct event
 {
@@ -42,8 +72,8 @@ struct process* create_process(char* id);
 void enqueue(struct process **queue_tail, struct process *new_process);
 struct process* dequeue(struct process **queue_head);
 void process_arrival(struct process **cpu_queue_tail, char* id);
-void cpu_start(struct process **cpu_queue_head, struct process **disk_tail);
-void cpu_finish(struct process **cpu_queue_head, struct process **disk_tail);
+//void cpu_start(struct process **cpu_queue_head, struct process **disk_tail);  TODO: Flesh out or remove
+//void cpu_finish(struct process **cpu_queue_head, struct process **disk_tail);
 void printToOutput(FILE *output, struct event *current_event);
 
 
@@ -60,7 +90,6 @@ int SEED,
     DISK1_MAX,
     DISK2_MIN,
     DISK2_MAX;
-
 
 void main ()
 {
@@ -106,7 +135,7 @@ void main ()
     struct event *end_event = (struct event*) malloc(sizeof(struct event));
     strcpy(end_event->eventid, "END");
     end_event->poptime = FIN_TIME;
-    end_event->eventtype = 9;
+    end_event->eventtype = 0;
     end_event->prev = start_event;
     start_event->next = end_event;
 
@@ -223,17 +252,20 @@ void main ()
         sleep(1);
             break;
         }
- //       sprintf(str, "%d", processcount);
- //       sorted_event_enqueue(&event_queue_root, create_event(str, 
- //           globaltime + ranged_rand(ARRIVE_MAX,ARRIVE_MIN), 1)); 
         free(current_event);
     }
     fclose(output);
 }
 
+/*  initialize_from_file reads in the globals from the input file.
+    I think there is a way to change this into a header so that I
+    can keep these all as constants.
+
+    TODO: Try to refactor this into a header file
+*/
 void initialize_from_file()
 {
-        char inputfilename[] = "initializers.dat";
+    char inputfilename[] = "initializers.dat";
     FILE *input;
 
     input = fopen(inputfilename, "r");
@@ -256,13 +288,35 @@ void initialize_from_file()
     fscanf(input, "%*s %d", &DISK2_MAX);
 }
 
-/*  Exists to slightly clean up the code when a random is called  */
+/*  ranged_rand exists to slightly clean up the code when a random is called  
 
+    Arguments:
+    max - An int representing the maximum value of the range of randoms you want
+
+    min - An int representing the minimum value of the range of randoms you want
+
+    Return:
+    An int between the range of min - max
+*/
 int ranged_rand(int max, int min)
 {
     return (rand() % (max - min + 1) + min);
 }
 
+/*  popevent removes the event at the top of the event priority queue.
+    That event is assumed to be the event with the lowest poptime but
+    this method does not check to ensure that.  After popping the event it
+    checks if the event queue is empty and if not, sets the next event as the new root.
+
+    TODO: This check shouldn't be needed, because there should never be a single event
+    .      It was from earlier unit testing.  Consider removing.
+
+    Arguments:
+    event_queue_root - a pointer to a pointer to the root node of the event queue
+
+    Return:
+    A pointer to the popped event 
+*/
 struct event* popevent(struct event **event_queue_root)
 {
     struct event *old_root = *event_queue_root;
@@ -275,6 +329,15 @@ struct event* popevent(struct event **event_queue_root)
     return old_root; 
 }
 
+/*  print_event prints an event structures data to the terminal.
+    It was used for debugging and testing
+
+    Arguments:
+    current_event - a pointer to an event structure whose data will be printed
+
+    Return:
+    nothing
+*/
 void print_event(struct event *current_event)
 {
     printf("\nThe event id is %s", current_event->eventid);
@@ -284,6 +347,18 @@ void print_event(struct event *current_event)
     sleep(1);
 }
 
+/*  print_created prints an event structures data to the terminal.
+    It was used for debugging and testing.
+    It only differs from print_event in that it prints "*****" before the
+    information.  This difference should probably have been an argument not
+    a seperate method
+
+    Arguments:
+    current_event - a pointer to an event structure whose data will be printed
+
+    Return:
+    nothing
+*/
 void print_created(struct event *current_event)
 {
     printf("\n*****The event id is %s", current_event->eventid);
@@ -293,6 +368,20 @@ void print_created(struct event *current_event)
     sleep(1);
 }
 
+/*  create_event handles the creation of new events.
+    It contains the actual malloc command and the initializing of the
+    event variables.
+
+    Arguments:
+    id - a string to identify the process associated with the event
+
+    time - the time the event will occur, used for sorting events
+
+    type - the event type.  The various event types are listed previously
+
+    Return:
+    a point to the newly created event
+*/
 struct event* create_event(char* id, int time, int type)
 {
     struct event *new_event = (struct event*) malloc(sizeof(struct event));
@@ -307,6 +396,22 @@ struct event* create_event(char* id, int time, int type)
     return new_event;
 }
 
+/*  sorted_event_enqueue inserts events into the event queue according to their poptime
+    If the time matches the lowest time of the system, that event becomes the new root.
+    This means that newer events occur prior to older events of the same poptime.  Therefore
+    the END event created at the start actually occurs last amongst all events at the end
+    time.
+
+    TODO: check if that's play with a TA
+
+    Arguments:
+    event_queue_root - a pointer to a pointer to the root node of the event queue
+
+    new_event - a pointer to the event node to be inserted into the queue
+
+    Return:
+    Nothing
+*/
 void sorted_event_enqueue(struct event **event_queue_root, struct event *new_event)
 {
     struct event *traverser = (*event_queue_root);
@@ -338,6 +443,15 @@ void sorted_event_enqueue(struct event **event_queue_root, struct event *new_eve
     } 
 }
 
+/*  create_process handles the creation of new process nodes
+    Contains the actual malloc command then initializes the id
+
+    Arguments:
+    id - a string containing the id of the new process
+
+    Return:
+    a pointer the the newly created process node
+*/
 struct process* create_process(char* id)
 {
     struct process *new_process = (struct process*) malloc(sizeof(struct process));
@@ -345,6 +459,16 @@ struct process* create_process(char* id)
     return new_process;
 }
 
+/*  enqueue adds a process node to the end of a given queue
+
+    Arguments:
+    queue_tail - a pointer to a pointer to the tail node in a queue
+
+    new_process - a pointer to the process node being enqueued
+
+    Return:
+    nothing
+*/
 void enqueue(struct process **queue_tail, struct process *new_process)
 {
     if ((*queue_tail) != NULL)
@@ -361,6 +485,15 @@ void enqueue(struct process **queue_tail, struct process *new_process)
     
 }
 
+/*  dequeue removes the first node in a queue.
+    Then moves the root to the next node back
+
+    Arguments:
+    queue_head - pointer to a pointer to the root process node of a queue
+
+    Return:
+    pointer to the removed process node
+*/
 struct process* dequeue(struct process **queue_head)
 {
     struct process *popped_process = (*queue_head);
@@ -369,12 +502,25 @@ struct process* dequeue(struct process **queue_head)
     return popped_process;
 }
 
+/*  process_arrival handles the actions to be carried out for an arrival event
+
+    TODO: Started this abstraction to clean up main, never finished.  FINISH
+
+    Arguments:
+    cpu_queue_tail - a pointer to a pointer to the tail node of the cpu queue
+
+    id - string containing the process id
+
+    Return:
+    nothing
+*/
 void process_arrival(struct process **cpu_queue_tail, char* id)
 {
     struct process *new_process = create_process(id);
     enqueue(cpu_queue_tail, new_process);
 }
 
+/*  TODO: Flesh out or remove
 void cpu_is_empty(struct process **cpu_queue_head, struct process **disk_tail)
 {
 
@@ -390,12 +536,27 @@ void cpu_finish(struct process **cpu_queue_head, struct process **disk_tail)
         enqueue(disk_tail, finished_process);
     }
 }
+*/
 
+/*  printToOutput handles the log file output
+    Initializes a string depending on the eventtype.
+    Uses that string in unchanged fprintf
+
+    Arguments:
+    output - pointer to the output file object
+
+    curent_event - pointer to the event to be printed
+
+    Return:
+    nothing
+*/
 void printToOutput(FILE *output, struct event *current_event)
 {
     char printType[22];
     switch (current_event->eventtype)
     {
+    case 0: strcpy(printType, "ENDING SIM");
+        break;
     case 1: strcpy(printType, "STARTING SIM");
         break;
     case 2: strcpy(printType, "arriving at cpu queue");
@@ -412,7 +573,7 @@ void printToOutput(FILE *output, struct event *current_event)
         break;
     case 8: strcpy(printType, "leaving DISK2");
         break;
-    case 9: strcpy(printType, "");
+    case 9: strcpy(printType, "leaving simulation");
         break;
     }
     fprintf(output, "at time %d process %s is %s\n\n", current_event->poptime, current_event->eventid, printType);
